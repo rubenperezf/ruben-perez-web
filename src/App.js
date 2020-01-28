7b9fafad2d3e6a00171a873bf54a5fac1b59a323
rubenperezf <rubenperezfeijoo@gmail.com>
2020-01-28 17:12:58 +0100

    with react ourter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import "./styles/App.css";
 import "./styles/Header.css"
 import "./styles/Welcome.css"
 import "./styles/Portafolio.css"
 import "./styles/Likes.css"
 import "./styles/Footer.css"
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./utils/history";
 
 import Header from "./components/Header"
 import Welcome from "./components/Welcome"
@@ -15,11 +18,13 @@ import Footer from "./components/Footer"
 function App() {
   return (
     <div className="App">
+      <Router history={history}>
       <Header />
       <Welcome/>
       <Portafolio/>
       <Likes/>
       <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,6 @@
 import React from "react"
 import RubenWelcome from "../images/welcome/ruben-welcome.jpg"
+import {Link} from "react-router-dom"
 
 function Welcome() {
     return (
@@ -8,9 +9,8 @@ function Welcome() {
             <div className="text">
                 <p>Hi! I'm</p>
                 <h1>Ruben Perez</h1>
-
-
             </div>
+
             <div class="box-1">
   <div class="btn btn-one">
     <span>HOVER ME</span>
@@ -19,7 +19,7 @@ function Welcome() {
     <span>HOVER ME</span>
   </div>
   <div class="btn btn-one">
-    <span>HOVER ME</span>
+    <Link to="./Likes"><span>HOVER ME</span></Link>
   </div>
   
 </div>

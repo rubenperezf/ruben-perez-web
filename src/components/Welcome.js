89d1d98f3a70f211341887e054c4ffa56deaa4e1
rubenperezf <rubenperezfeijoo@gmail.com>
2020-01-28 16:42:53 +0100

    without yarn

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,13 +3,30 @@ import RubenWelcome from "../images/welcome/ruben-welcome.jpg"
 
 function Welcome() {
     return (
+   
         <div className="welcome-container" style={{backgroundImage: `url(${RubenWelcome})`}}>
             <div className="text">
                 <p>Hi! I'm</p>
                 <h1>Ruben Perez</h1>
+
+
             </div>
+            <div class="box-1">
+  <div class="btn btn-one">
+    <span>HOVER ME</span>
+  </div>
+  <div class="btn btn-one">
+    <span>HOVER ME</span>
+  </div>
+  <div class="btn btn-one">
+    <span>HOVER ME</span>
+  </div>
+  
+</div>
 
         </div>
+
+
     )
 }
 

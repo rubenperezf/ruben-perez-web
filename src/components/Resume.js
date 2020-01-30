bc3dd8eba5b1444fab87d0cfdbc280367387e864
rubenperezf <rubenperezfeijoo@gmail.com>
2020-01-30 02:47:04 +0100

    resume done

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,99 @@ import React from "react"
 function Resume() {
     return (
         <div className="resume-container" id="resume">
-            <p>hola</p>
+            <h1>Resume</h1>
+            <ul class="timeline">
+
+
+            <li>
+    <div class="direction-r">
+        <div class="flag-wrapper">
+            <span class="flag">JRS Coding School</span>
+            <span class="time-wrapper"><span class="time">Oct.2019 - Jan.2020</span></span>
+        </div>
+        <div class="desc">High School Spanish Teacher.</div>
+    </div>
+</li>
+
+
+<li>
+    <div class="direction-l">
+        <div class="flag-wrapper">
+            <span class="flag">Spanish Teacher</span>
+            <span class="time-wrapper"><span class="time">March.2017 - March.2019</span></span>
+        </div>
+        <div class="desc">High School Spanish Teacher. Charleston Countyy School District.</div>
+    </div>
+</li>
+
+
+<li>
+    <div class="direction-r">
+        <div class="flag-wrapper">
+            <span class="flag">Volunteering</span>
+            <span class="time-wrapper"><span class="time">Sept.2016 - March.2017 </span></span>
+        </div>
+        <div class="desc">Helping at Watermission. Charleston</div>
+    </div>
+</li>
+<li>
+    <div class="direction-l">
+        <div class="flag-wrapper">
+            <span class="flag">Don't Be a Foreinger</span>
+            <span class="time-wrapper"><span class="time">Feb.2014 - Aug.2016 </span></span>
+        </div>
+        <div class="desc">Founded a summer exchange program for students in Spain to visit Charleston.</div>
+    </div>
+</li>
+<li>
+    <div class="direction-r">
+        <div class="flag-wrapper">
+            <span class="flag">Master of Inter. Business</span>
+            <span class="time-wrapper"><span class="time">2012 - 2013 </span></span>
+        </div>
+        <div class="desc">IFFE Business School, La Coruña, Spain.</div>
+    </div>
+</li>
+<li>
+    <div class="direction-l">
+        <div class="flag-wrapper">
+            <span class="flag">Ges Seguros</span>
+            <span class="time-wrapper"><span class="time">Nov.2011-Nov.2012</span></span>
+        </div>
+        <div class="desc">Working in Insurances in Spain.</div>
+    </div>
+</li>
+<li>
+    <div class="direction-r">
+        <div class="flag-wrapper">
+            <span class="flag">Master Secondary Education</span>
+            <span class="time-wrapper"><span class="time">2010-2011</span></span>
+        </div>
+        <div class="desc">Universidad de Santiago de Compostela, Spain</div>
+    </div>
+</li>
+<li>
+    <div class="direction-l">
+        <div class="flag-wrapper">
+            <span class="flag">B.S.Business Administrations</span>
+            <span class="time-wrapper"><span class="time">2002-2009</span></span>
+        </div>
+        <div class="desc">Universidad de Santiago de Compostela, Spain</div>
+    </div>
+</li>
+<li>
+    <div class="direction-r">
+        <div class="flag-wrapper">
+            <span class="flag">Intermediate Degree in Music</span>
+            <span class="time-wrapper"><span class="time">1995-2005</span></span>
+        </div>
+        <div class="desc">Lugo, Spain</div>
+    </div>
+</li>
+
+
+</ul>
+
         </div>
     )
 }

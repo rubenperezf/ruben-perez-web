9e516475c8357ab315abc851e83d4e5c636d3d62
rubenperezf <rubenperezfeijoo@gmail.com>
2020-01-31 00:22:57 +0100

    social media

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer-container">
-
+        <h1>Let me show what I am be able to do.</h1>
+        <p>&copy; 2020 Ruben Perez</p>
     </div>
   );
 }
